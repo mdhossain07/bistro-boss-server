82371ab68654397b8f84d53deca0fa76bdbb32a9
mdhossain07 <mrabir8097@gmail.com>
2023-11-15 08:59:15 +0600

    delete api created

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esabfel.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -24,6 +24,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
     app.get("/api/v1/get-menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -32,6 +33,28 @@ async function run() {
 
     app.get("/api/v1/get-review", async (req, res) => {
       const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Cart Collection
+
+    app.post("/api/v1/create/food-item", async (req, res) => {
+      const food = req.body;
+      const result = await cartCollection.insertOne(food);
+      res.send(result);
+    });
+
+    app.get("/api/v1/get-cart", async (req, res) => {
+      const { email } = req.query;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/api/v1/delete-item/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
